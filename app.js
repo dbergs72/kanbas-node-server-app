63e6ac84b203f8612a027e9c62352a870d14149d
dbergs72 <dbergs72@gmail.com>
2023-11-09 16:42:20 -0500

    wip

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }),
+);
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
--- modules/routes.js
+++ modules/routes.js
@@ -1,41 +1,38 @@
 import db from "../Database/index.js";
 
 function ModuleRoutes(app) {
-    app.put("/api/modules/:mid", (req, res) => {
-        const {mid} = req.params;
-        const moduleIndex = db.modules.findIndex(
-            (m) => m._id === mid);
-        db.modules[moduleIndex] = {
-            ...db.modules[moduleIndex],
-            ...req.body
-        };
-        res.sendStatus(204);
-    });
+  app.put("/api/modules/:mid", (req, res) => {
+    const { mid } = req.params;
+    const moduleIndex = db.modules.findIndex((m) => m._id === mid);
+    db.modules[moduleIndex] = {
+      ...db.modules[moduleIndex],
+      ...req.body,
+    };
+    res.sendStatus(204);
+  });
 
-    app.delete("/api/modules/:mid", (req, res) => {
-        const {mid} = req.params;
-        db.modules = db.modules
-            .filter((m) => m._id !== mid);
-        res.sendStatus(200);
-    });
+  app.delete("/api/modules/:mid", (req, res) => {
+    const { mid } = req.params;
+    db.modules = db.modules.filter((m) => m._id !== mid);
+    res.sendStatus(200);
+  });
 
-    app.post("/api/courses/:cid/modules", (req, res) => {
-        const {cid} = req.params;
-        const module = {
-            ...req.body,
-            _id: new Date().getTime().toString(),
-            course: cid
-        };
-        db.modules.push(module);
-        res.send(module);
-    });
+  app.post("/api/courses/:cid/modules", (req, res) => {
+    const { cid } = req.params;
+    const module = {
+      ...req.body,
+      _id: new Date().getTime().toString(),
+      course: cid,
+    };
+    db.modules.push(module);
+    res.send(module);
+  });
 
-    app.get("/api/courses/:cid/modules", (req, res) => {
-        const {cid} = req.params;
-        const modules = db.modules
-            .filter((m) => m.course === cid);
-        res.send(modules);
-    });
+  app.get("/api/courses/:cid/modules", (req, res) => {
+    const { cid } = req.params;
+    const modules = db.modules.filter((m) => m.course === cid);
+    res.send(modules);
+  });
 }
 
 export default ModuleRoutes;
