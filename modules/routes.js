fe229a6260942c183443ea122e1a1de935c59508
dbergs72 <dbergs72@gmail.com>
2023-11-09 14:02:30 -0500

    4.3.3

--- modules/routes.js
+++ modules/routes.js
@@ -1,7 +1,15 @@
 import db from "../Database/index.js";
+
 function ModuleRoutes(app) {
+    app.delete("/api/modules/:mid", (req, res) => {
+        const {mid} = req.params;
+        db.modules = db.modules
+            .filter((m) => m._id !== mid);
+        res.sendStatus(200);
+    });
+
     app.post("/api/courses/:cid/modules", (req, res) => {
-        const { cid } = req.params;
+        const {cid} = req.params;
         const module = {
             ...req.body,
             _id: new Date().getTime().toString(),
@@ -12,9 +20,11 @@ function ModuleRoutes(app) {
     });
 
     app.get("/api/courses/:cid/modules", (req, res) => {
-        const { cid } = req.params;
+        const {cid} = req.params;
         const modules = db.modules
             .filter((m) => m.course === cid);
         res.send(modules);
-    }); }
+    });
+}
+
 export default ModuleRoutes;
